deca1027da25d4e361041355db36d71e06282d1e
Tim Spence <timothywspence@gmail.com>
2023-01-31 11:22:46 +0000

    Respond with Pong

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 // Uncomment this block to pass the first stage
+use std::io::Write;
 use std::net::TcpListener;
+mod model;
+use model::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                _stream.write(&Response::Pong.encode()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
---
+++ src/model.rs
@@ -0,0 +1,15 @@
+pub enum Response {
+    Pong,
+}
+
+impl RESPEncoder for Response {
+    fn encode(&self) -> Vec<u8> {
+        match self {
+            Response::Pong => "+PONG".as_bytes().to_vec()
+        }
+    }
+}
+
+pub trait RESPEncoder {
+    fn encode(&self) -> Vec<u8>;
+}
