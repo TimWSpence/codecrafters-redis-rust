992cfa97e80649da73da41be82301e69101b853d
Tim Spence <timothywspence@gmail.com>
2023-02-03 11:01:53 +0000

    Handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::BufReader;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 mod model;
 use model::*;
 
@@ -11,7 +12,10 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                handle_connection(_stream);
+                thread::spawn(|| {
+                    handle_connection(_stream);
+                });
+                ()
             }
             Err(e) => {
                 println!("error: {}", e);
